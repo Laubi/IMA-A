03977bee72d8ba1962c13424ca9ab76f647e8309
David Laubreiter <david.laubreiter@outlook.com>
2018-02-01 23:44:08 +0100

    Remove unused constructor

--- app/src/main/java/at/laubi/finalproject/components/LoadingImage.kt
+++ app/src/main/java/at/laubi/finalproject/components/LoadingImage.kt
@@ -8,7 +8,6 @@ class LoadingImage: ImageView {
     constructor(context: Context?) : super(context)
     constructor(context: Context?, attrs: AttributeSet?) : super(context, attrs)
     constructor(context: Context?, attrs: AttributeSet?, defStyleAttr: Int) : super(context, attrs, defStyleAttr)
-    constructor(context: Context?, attrs: AttributeSet?, defStyleAttr: Int, defStyleRes: Int) : super(context, attrs, defStyleAttr, defStyleRes)
 
     init{
         setImageDrawable(context.getDrawable(android.R.drawable.ic_menu_rotate))
