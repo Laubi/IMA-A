d5aad25582312cc62f9b46b67c4112e08d5eb03d
David <david.laubreiter@outlook.com>
2018-02-01 23:36:31 +0100

    Remove animation from LoadingImage

--- app/src/main/java/at/laubi/finalproject/components/LoadingImage.kt
+++ app/src/main/java/at/laubi/finalproject/components/LoadingImage.kt
@@ -1,16 +1,10 @@
 package at.laubi.finalproject.components
 
 import android.content.Context
-import android.graphics.Bitmap
 import android.util.AttributeSet
-import android.view.animation.Animation
-import android.view.animation.LinearInterpolator
-import android.view.animation.RotateAnimation
 import android.widget.ImageView
 
 class LoadingImage: ImageView {
-    private val animation = RotateAnimation(0f, 360f, Animation.RELATIVE_TO_SELF, .5f, Animation.RELATIVE_TO_SELF, .5f)
-
     constructor(context: Context?) : super(context)
     constructor(context: Context?, attrs: AttributeSet?) : super(context, attrs)
     constructor(context: Context?, attrs: AttributeSet?, defStyleAttr: Int) : super(context, attrs, defStyleAttr)
@@ -18,16 +12,5 @@ class LoadingImage: ImageView {
 
     init{
         setImageDrawable(context.getDrawable(android.R.drawable.ic_menu_rotate))
-
-        animation.interpolator = LinearInterpolator()
-        animation.repeatCount = Animation.INFINITE
-        animation.duration = 2000
-
-        startAnimation(animation)
-    }
-
-    override fun setImageBitmap(bm: Bitmap?) {
-        clearAnimation()
-        super.setImageBitmap(bm)
     }
 }
