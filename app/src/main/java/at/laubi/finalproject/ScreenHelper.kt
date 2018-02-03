c3e5f6a9f46ae305e858fe68c12e462db6363fee
David Laubreiter <david.laubreiter@outlook.com>
2018-02-03 22:20:21 +0100

    ScreenSize should returns not a Size object

--- app/src/main/java/at/laubi/finalproject/ScreenHelper.kt
+++ app/src/main/java/at/laubi/finalproject/ScreenHelper.kt
@@ -1,10 +1,12 @@
 package at.laubi.finalproject
 
+import android.graphics.Point
 import android.util.DisplayMetrics
+import android.util.Size
 import android.view.WindowManager
 
-fun getScreenSize(wm: WindowManager): DisplayMetrics {
-    val metrics = DisplayMetrics()
-    wm.defaultDisplay.getMetrics(metrics)
-    return metrics
+fun getScreenSize(wm: WindowManager): Size {
+    val metrics = Point()
+    wm.defaultDisplay.getSize(metrics)
+    return Size(metrics.x, metrics.y)
 }
