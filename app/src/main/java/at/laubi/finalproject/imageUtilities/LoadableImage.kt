4e849847ad8ff9c5feae7cb6e7454132f279971f
David Laubreiter <david.laubreiter@outlook.com>
2018-02-04 00:08:31 +0100

    Create LoadableImageLoader

--- app/src/main/java/at/laubi/finalproject/imageUtilities/LoadableImage.kt
+++ app/src/main/java/at/laubi/finalproject/imageUtilities/LoadableImage.kt
@@ -1,9 +1,15 @@
 package at.laubi.finalproject.imageUtilities
 
+import android.content.AsyncTaskLoader
 import android.content.ContentResolver
+import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.net.Uri
+import android.os.AsyncTask
 import android.provider.MediaStore.Images.Media.*
 import android.util.Size
+import at.laubi.finalproject.cache.DiskBitmapCache
 import java.io.File
 
 
@@ -44,6 +50,73 @@ private fun resolveImages(contentResolver: ContentResolver, selection: String?, 
 
     return results
 }
+
+internal class LoadableImageLoader constructor(
+        val contentResolver: ContentResolver,
+        val image: LoadableImage,
+        val targetSize: Size,
+        val listener: (bm: Bitmap?) -> Unit
+){
+    fun load(){
+        Executor().execute()
+    }
+
+    // We create an inner class, so the AsyncTask will not leak outside
+    private inner class Executor: AsyncTask<Void, Void, Bitmap>() {
+        override fun onPostExecute(result: Bitmap?) {
+            listener(result)
+        }
+
+        override fun doInBackground(vararg p0: Void?): Bitmap {
+            val cachedBitmap = tryLoadBitmapFromCache(image.cacheId)
+
+            return if(cachedBitmap != null){
+                cachedBitmap
+            }else{
+                val options = BitmapFactory.Options()
+                options.inScaled = true
+                options.inSampleSize = calculateSampleSize(targetSize)
+
+                val stream = contentResolver.openInputStream(image.uri)
+                val bm = BitmapFactory.decodeStream(stream, null, options)
+                stream.close()
+
+                tryStoreBitmapInCache(image.cacheId, bm)
+
+                bm
+            }
+        }
+    }
+
+    private fun tryLoadBitmapFromCache(key: String): Bitmap?{
+        val cache = DiskBitmapCache.getInstance()
+        return cache?.loadBitmap(key)
+    }
+
+    private fun tryStoreBitmapInCache(key: String, bitmap: Bitmap){
+        val cache = DiskBitmapCache.getInstance()
+        cache?.storeBitmap(bitmap, key)
+    }
+
+    private fun calculateSampleSize(target: Size): Int{
+        var sampleSize = 1
+
+        if(image.height > target.height || image.width > target.width){
+            val hh = image.height / 2
+            val hw = image.width / 2
+
+            while(
+                    (hh / sampleSize) > target.height &&
+                    (hw / sampleSize) > target.width){
+                sampleSize *= 2
+            }
+        }
+
+        return sampleSize
+    }
+}
+
+
 
 class LoadableImage internal constructor(
         val id: Long,
