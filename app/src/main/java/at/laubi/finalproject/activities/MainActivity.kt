d08948f95f92225ae97401d5af9c6de53eda8a9b
David Laubreiter <david.laubreiter@outlook.com>
2018-02-04 16:38:09 +0100

    Add permission check

--- app/src/main/java/at/laubi/finalproject/activities/MainActivity.kt
+++ app/src/main/java/at/laubi/finalproject/activities/MainActivity.kt
@@ -1,7 +1,10 @@
 package at.laubi.finalproject.activities
 
+import android.Manifest.permission.READ_EXTERNAL_STORAGE
 import android.app.Activity
 import android.content.Intent
+import android.content.pm.PackageManager.PERMISSION_DENIED
+import android.content.pm.PackageManager.PERMISSION_GRANTED
 import android.os.Bundle
 import android.widget.*
 import at.laubi.finalproject.*
@@ -9,13 +12,31 @@ import at.laubi.finalproject.cache.DiskBitmapCache
 import at.laubi.finalproject.adapters.CustomImageAdapter
 import at.laubi.finalproject.imageUtilities.*
 
+private const val PERMISSION_REQUEST_CODE = 0x6482
+private val REQUIRED_PERMISSIONS = arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE)
 
 class MainActivity : Activity() {
-    private lateinit var layout: GridView
-    private lateinit var adapter: CustomImageAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        if(checkPermissions()){
+            showImageContents()
+        }else{
+            showNeedPermissions()
+        }
+    }
+
+    private fun showNeedPermissions(){
+        setContentView(R.layout.activity_main_no_permission)
+
+        val button = findViewById<Button>(R.id.requestPermissionsButton)
+        button.setOnClickListener {
+            checkPermissions()
+        }
+    }
+
+    private fun showImageContents(){
         setContentView(R.layout.activity_main)
 
         // Initialize DiskBitmapCache
@@ -23,9 +44,9 @@ class MainActivity : Activity() {
 
         val images = LoadableImage.all(contentResolver)
 
-        adapter = CustomImageAdapter(this, images)
+        val adapter = CustomImageAdapter(this, images)
 
-        layout = this.findViewById(R.id.gridView)
+        val layout = this.findViewById<GridView>(R.id.gridView)
         layout.adapter = adapter
         layout.numColumns = (screenSize.width / 100) + 1
 
@@ -34,6 +55,31 @@ class MainActivity : Activity() {
             intent.putExtra("ID", ID)
             startActivity(intent)
         }
+    }
+
+    private fun checkPermissions(): Boolean{
+        val hasAllRequiredPermissions =
+                REQUIRED_PERMISSIONS
+                        .map(this::checkSelfPermission).none { result -> result ==  PERMISSION_DENIED }
+
+        if(!hasAllRequiredPermissions){
+            requestPermissions(arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE), PERMISSION_REQUEST_CODE)
+        }
+
+        return hasAllRequiredPermissions
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>?, grantResults: IntArray) {
+        when(requestCode){
+            PERMISSION_REQUEST_CODE -> {
+                if( grantResults.isNotEmpty() && grantResults.first() == PERMISSION_GRANTED){
+                    showImageContents()
+                }else{
+                    showNeedPermissions()
+                }
+            }
+        }
+
     }
 }
 
