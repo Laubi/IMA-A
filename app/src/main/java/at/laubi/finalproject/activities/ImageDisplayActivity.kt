8ad86c84f3e16412ccd3fb6a3fa2d4b6eadd18c5
David Laubreiter <david.laubreiter@outlook.com>
2018-02-04 15:44:36 +0100

    Change implementation for the use of GestureImageView

--- app/src/main/java/at/laubi/finalproject/activities/ImageDisplayActivity.kt
+++ app/src/main/java/at/laubi/finalproject/activities/ImageDisplayActivity.kt
@@ -3,20 +3,15 @@ package at.laubi.finalproject.activities
 import android.app.Activity
 import android.graphics.Bitmap
 import android.os.Bundle
-import android.view.GestureDetector
-import android.view.MotionEvent
+import android.util.Size
 import android.widget.ImageView
 import at.laubi.finalproject.R
-import at.laubi.finalproject.getScreenSize
 import at.laubi.finalproject.imageUtilities.LoadableImage
-import nl.dionsegijn.pixelate.Pixelate
+import at.laubi.finalproject.screenSize
 
 class ImageDisplayActivity : Activity() {
     private lateinit var imageView: ImageView
     private lateinit var targetBitmap: Bitmap
-
-    private lateinit var gestureDetector: GestureDetector
-    private val gestureListener: GestureDetector.OnGestureListener
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,43 +19,16 @@ class ImageDisplayActivity : Activity() {
         actionBar.setDisplayHomeAsUpEnabled(true)
 
         imageView = findViewById(R.id.imageView)
-        gestureDetector = GestureDetector(this, gestureListener)
-
 
         val image = LoadableImage.byId(contentResolver, intent.extras.getLong("ID"))
 
-        image?.load(getScreenSize(windowManager), imageView::setImageBitmap)
-    }
+        val targetSize = screenSize
 
-    override fun onTouchEvent(event: MotionEvent?): Boolean {
-        gestureDetector.onTouchEvent(event)
-        return super.onTouchEvent(event)
-    }
-
-    init{
-        gestureListener = object: GestureDetector.SimpleOnGestureListener(){
-            override fun onScroll(initial: MotionEvent?, current: MotionEvent?, distanceX: Float, distanceY: Float): Boolean {
-
-                if(distanceX > distanceY){
-                    current?.let{
-                        val x_move = it.x.div(getScreenSize(windowManager).width)
-
-                        pixelateImage((targetBitmap.width * x_move).toInt())
-                    }
-                }
-
-                return true
+        image?.load(Size(targetSize.width / 2, targetSize.height / 2), { bm ->
+            bm?.let{
+                targetBitmap = it
+                imageView.setImageBitmap(it)
             }
-        }
-    }
-
-    private fun pixelateImage(blocks: Int){
-        Pixelate(targetBitmap)
-                .setDensity(100)
-                .setListener { bitmap, density ->
-                    targetBitmap = bitmap
-                    imageView.setImageBitmap(bitmap)
-                }
-                .make()
+        })
     }
 }
